2cced7036ed86542d5c0ed189a3e00e4ced9912f
nnayak9609-byte <nnayak9609@gmail.com>
2026-04-14 15:34:18 +0530

    Update rota-logic.js

--- rota-logic.js
+++ rota-logic.js
@@ -1,15 +1,15 @@
 function autoSwitchRota() {
-    let currentDay = parseInt(localStorage.getItem('dayNumber')) || 1;
-    let currentRota = parseInt(localStorage.getItem('rota_num')) || 1;
-    let rotaLength = 8;
+    var currentDay = parseInt(localStorage.getItem('dayNumber')) || 1;
+    var currentRota = parseInt(localStorage.getItem('rota_num')) || 1;
+    var rotaLength = 8;
 
     if (currentDay > rotaLength) {
-        let nextRota = currentRota + 1;
+        var nextRota = currentRota + 1;
         
         localStorage.setItem('rota_num', nextRota.toString());
         localStorage.setItem('dayNumber', '1');
         
-        alert(`Transitioning to Rota ${nextRota}`);
+        alert("Transitioning to Rota " + nextRota);
         location.reload();
     }
 }
